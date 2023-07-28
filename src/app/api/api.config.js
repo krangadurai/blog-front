2db91180ec4d6de0496c5587f41213dfc6361d38
krangadurai <rangadurai27@gmail.com>
2023-07-28 16:38:43 +0530

    httpCookie request enable

--- src/app/api/api.config.js
+++ src/app/api/api.config.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: 'http://localhost:8080', // Replace with your actual API base URL
+  withCredentials: true,
 });
 
 // api.interceptors.request.use(
@@ -24,4 +25,4 @@ const api = axios.create({
 // );
 
 
-export default api
+export default api;
